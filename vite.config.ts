fb92eef3265d8b6c15f863683b7e569148cc27d9
treere <tomasiandrea.at@gmail.com>
2026-03-11 00:55:45 +0100

    Remove warnings

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,21 @@ export default defineConfig({
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('svelte/src/internal')) {
+            return 'svelte-internal';
+          }
+        }
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
+        silenceDeprecations: ['legacy-js-api'],
         additionalData: `@use './src/style/style.scss';
 `
       }
